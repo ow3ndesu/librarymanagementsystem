160e21abb13c14c3f025f3c27359278c4dff73ea
Ow3ndesu <ow3ndesu@users.noreply.github.com>
2022-11-26 04:46:59 +0800

    notifications

--- process/dashboard.process.php
+++ process/dashboard.process.php
@@ -36,4 +36,37 @@ class Process extends Database
             echo 'EXECUTION_ERROR';
         }
     }
+
+    public function LoadNotifications()
+    {
+        $notifications = [];
+        $stmt = $this->conn->prepare("SELECT b.*, s.firstname FROM borrowals b INNER JOIN students s ON b.student_id = s.student_id WHERE status = 'PENDING' OR status = 'RETURNING' ORDER BY b.id DESC;");
+        $stmt1 = $this->conn->prepare("SELECT u.* FROM users u WHERE u.status = 'DISABLED' AND u.user_id NOT IN (SELECT s.user_id FROM students s) ORDER BY u.user_id DESC;");
+
+        if ($stmt->execute()) {
+            $result = $stmt->get_result();
+            $stmt->close();
+            
+            if ($stmt1->execute()) {
+                $result1 = $stmt1->get_result();
+                $stmt1->close();
+                
+                while ($row1 = $result1->fetch_assoc()) {
+                    array_push($notifications, $row1);
+                }
+
+                while ($row = $result->fetch_assoc()) {
+                    array_push($notifications, $row);
+                }
+                
+                echo json_encode(array(
+                    "NOTIFICATIONS" => $notifications
+                ));
+            } else {
+                echo 'EXECUTION_ERROR';
+            }
+        } else {
+            echo 'EXECUTION_ERROR';
+        }
+    }
 }
--- routes/dashboard.route.php
+++ routes/dashboard.route.php
@@ -5,3 +5,7 @@ $process = new Process();
 if (isset($_POST["action"]) && $_POST["action"] == "LoadDashboard") {
     $process->LoadDashboard();
 }
+
+if (isset($_POST["action"]) && $_POST["action"] == "LoadNotifications") {
+    $process->LoadNotifications();
+}
