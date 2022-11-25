bfe117a55e0f26ae44690740fd2f69d678d332d2
Ow3ndesu <ow3ndesu@users.noreply.github.com>
2022-11-26 05:51:37 +0800

    database

--- database/lmsdb.sql
+++ database/lmsdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 18, 2022 at 11:12 PM
+-- Generation Time: Nov 25, 2022 at 10:50 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -57,6 +57,7 @@ CREATE TABLE `books` (
   `id` int(11) NOT NULL,
   `book_id` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL,
+  `copy` varchar(255) NOT NULL,
   `title` varchar(255) NOT NULL,
   `author` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
@@ -71,12 +72,13 @@ CREATE TABLE `books` (
 -- Dumping data for table `books`
 --
 
-INSERT INTO `books` (`id`, `book_id`, `image`, `title`, `author`, `description`, `quantity`, `status`, `remarks`, `inserted_by`, `inserted_at`) VALUES
-(21, 'BOOK000TFGQ10', 'the-book.jpg', 'The Book', 'Arter', 'Get Your Art', 76, 'ACTIVE', '-', 'ADMIN000001', '10/30/2022'),
-(23, 'BOOK000SLCNJ1', 'lunar-storm.jpeg', 'Lunar Storm', 'Stormy', 'Lunatic', 28, 'ACTIVE', '-', 'ADMIN000001', '10/30/2022'),
-(24, 'BOOK000PWLGF7', 'memory-imp.jpg', 'The Imperfections of Memory', 'Angelina', 'Memories', 5, 'ACTIVE', '-', 'ADMIN000001', '10/30/2022'),
-(25, 'BOOK000X5KCHV', 'we-lost.jpg', 'TheMemoryWeLost', 'Mike', 'Roberts', 5, 'ACTIVE', '-', 'ADMIN000001', '11/02/2022'),
-(28, 'BOOK000NMXZ37', 'BOOK000NMXZ37-kjbsakjbdkabv.jpg', 'YTS Guideline', 'YTS Team', 'How to do torrent?', 94, 'ACTIVE', '-', 'ADMIN000001', '11/16/2022');
+INSERT INTO `books` (`id`, `book_id`, `image`, `copy`, `title`, `author`, `description`, `quantity`, `status`, `remarks`, `inserted_by`, `inserted_at`) VALUES
+(21, 'BOOK000TFGQ10', 'the-book.jpg', 'BOOK000INEPM1-sad;jasd;.pdf', 'The Book', 'Arter', 'Get Your Art', 76, 'ACTIVE', '-', 'ADMIN000001', '10/30/2022'),
+(23, 'BOOK000SLCNJ1', 'lunar-storm.jpeg', 'BOOK000INEPM1-sad;jasd;.pdf', 'Lunar Storm', 'Stormy', 'Lunatic', 28, 'ACTIVE', '-', 'ADMIN000001', '10/30/2022'),
+(24, 'BOOK000PWLGF7', 'memory-imp.jpg', 'BOOK000INEPM1-sad;jasd;.pdf', 'The Imperfections of Memory', 'Angelina', 'Memories', 6, 'ACTIVE', '-', 'ADMIN000001', '10/30/2022'),
+(25, 'BOOK000X5KCHV', 'we-lost.jpg', 'BOOK000INEPM1-sad;jasd;.pdf', 'TheMemoryWeLost', 'Mike', 'Roberts', 5, 'ACTIVE', '-', 'ADMIN000001', '11/02/2022'),
+(28, 'BOOK000NMXZ37', 'BOOK000NMXZ37-kjbsakjbdkabv.jpg', 'BOOK000INEPM1-sad;jasd;.pdf', 'YTS Guideline', 'YTS Team', 'How to do torrent?', 93, 'ACTIVE', '-', 'ADMIN000001', '11/16/2022'),
+(29, 'BOOK000INEPM1', 'BOOK000INEPM1-sad;jasd;.jpg', 'BOOK000INEPM1-sad;jasd;.pdf', 'dasdasd', 'sad;jasd;', 'asdasddas', 3, 'ACTIVE', '-', 'ADMIN000001', '11/21/2022');
 
 -- --------------------------------------------------------
 
@@ -101,8 +103,10 @@ CREATE TABLE `borrowals` (
 --
 
 INSERT INTO `borrowals` (`id`, `borrow_id`, `book_id`, `student_id`, `status`, `filed`, `due`, `modified_by`, `modified_at`) VALUES
-(3, 'BORROW1S5A21CA5', 'BOOK000PWLGF7', 'STUD0003IAVZX', 'RETURNED', '11/04/2022', '11/14/2022', 'ADMIN000001', '11/08/2022'),
-(16, 'BORROW000GNXYH4', 'BOOK000PWLGF7', 'STUD0003IAVZX', 'RETURNED', '11/17/2022', '11/27/2022', 'ADMIN000001', '11/17/2022');
+(3, 'BORROW1S5A21CA5', 'BOOK000PWLGF7', 'STUD0003IAVZX', 'RETURNED', '11/04/2022', '11/14/2022', 'ADMIN000001', '11/26/2022'),
+(16, 'BORROW000GNXYH4', 'BOOK000PWLGF7', 'STUD0003IAVZX', 'RETURNED', '11/17/2022', '11/27/2022', 'ADMIN000001', '11/17/2022'),
+(17, 'BORROW000LVTZ5X', 'BOOK000INEPM1', 'STUD0003IAVZX', 'BORROWED', '11/21/2022', '12/01/2022', 'ADMIN000001', '11/26/2022'),
+(18, 'BORROW000GXMT5O', 'BOOK000NMXZ37', 'STUD0003IAVZX', 'BORROWED', '11/24/2022', '12/04/2022', 'ADMIN000001', '11/24/2022');
 
 -- --------------------------------------------------------
 
@@ -135,7 +139,9 @@ INSERT INTO `messages` (`id`, `student_id`, `message`, `reply_to`, `isAdmin`, `c
 (23, 'STUD0003IAVZX', 'I need something man', 18, 0, 'November 19, 2022 02:35am', NULL),
 (24, 'STUD000R4PVCS', 'u there?', 22, 1, 'November 19, 2022 02:38am', NULL),
 (25, 'STUD0003IAVZX', 'come get me', 23, 0, 'November 19, 2022 02:39am', NULL),
-(26, 'STUD0003IAVZX', 'okay', 25, 1, 'November 19, 2022 02:40am', NULL);
+(26, 'STUD0003IAVZX', 'okay', 25, 1, 'November 19, 2022 02:40am', NULL),
+(27, 'STUD000R4PVCS', 'hey jon', 24, 1, 'November 21, 2022 02:09pm', NULL),
+(28, 'STUD000R4PVCS', 'why?', 27, 0, 'November 21, 2022 02:09pm', NULL);
 
 -- --------------------------------------------------------
 
@@ -156,7 +162,8 @@ CREATE TABLE `returns` (
 
 INSERT INTO `returns` (`id`, `borrow_id`, `remarks`, `returned_at`) VALUES
 (10, 'BORROW1S5A21CA5', '', '11/08/2022'),
-(11, 'BORROW000GNXYH4', '', '11/17/2022');
+(11, 'BORROW000GNXYH4', '', '11/17/2022'),
+(12, 'BORROW1S5A21CA5', '', '11/26/2022');
 
 -- --------------------------------------------------------
 
@@ -183,7 +190,9 @@ CREATE TABLE `students` (
 
 INSERT INTO `students` (`id`, `student_id`, `user_id`, `firstname`, `middlename`, `lastname`, `address`, `contact_no`, `is_completed`, `created_at`) VALUES
 (1, 'STUD0003IAVZX', 4, 'Juan', 'Pablo', 'Dela Cruz', 'Pag-Asa', '0905985511', 1, '10/30/2022'),
-(3, 'STUD000R4PVCS', 6, 'Jon', 'Stark', 'Snow', 'Winterfell', '09067894410', 1, '11/03/2022');
+(3, 'STUD000R4PVCS', 6, 'Jon', 'Stark', 'Snow', 'Winterfell', '09067894410', 1, '11/03/2022'),
+(5, 'STUD000FCE17D', 7, '', '', '', '', '', 0, '11/26/2022'),
+(7, 'STUD000UCPR37', 9, '', '', '', '', '', 0, '11/26/2022');
 
 -- --------------------------------------------------------
 
@@ -195,6 +204,7 @@ CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `proof` varchar(255) NOT NULL,
   `user_type` varchar(45) NOT NULL,
   `status` varchar(45) NOT NULL COMMENT 'ENABLED, DISABLED',
   `created_at` varchar(45) NOT NULL
@@ -204,10 +214,12 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `email`, `password`, `user_type`, `status`, `created_at`) VALUES
-(1, 'admin@email.com', '59235f35e4763abb0b547bd093562f6e', 'ADMIN', 'ENABLED', '10/19/2022'),
-(4, 'user@email.com', 'b58c6f14d292556214bd64909bcdb118', 'Student', 'ENABLED', '10/30/2022'),
-(6, 'try@email.com', '035dee598ebb867ba96273606287f009', 'Student', 'ENABLED', '11/03/2022');
+INSERT INTO `users` (`user_id`, `email`, `password`, `proof`, `user_type`, `status`, `created_at`) VALUES
+(1, '@adminacc', '5b94c55fc69d8d32275862a930ebb46f', '@student4-536.jpg', 'ADMIN', 'ENABLED', '10/19/2022'),
+(4, '@student2', '2c1ddc2fc774a31cbe18e8b3575d62cc', '@student4-536.jpg', 'Student', 'ENABLED', '10/30/2022'),
+(6, '@student3', '80ec8e02a3987d28fe68894658400e8c', '@student4-536.jpg', 'Student', 'ENABLED', '11/03/2022'),
+(7, '@student1', 'fe96821ecf1191932f631d60e620e466', '@student4-536.jpg', 'Student', 'ENABLED', '11/24/2022'),
+(9, '@student4', '4d53590c0a24c82a7530d1eeccae9558', '@student4-256.png', 'Student', 'ENABLED', '11/26/2022');
 
 --
 -- Indexes for dumped tables
@@ -280,37 +292,37 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `borrowals`
 --
 ALTER TABLE `borrowals`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `returns`
 --
 ALTER TABLE `returns`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
